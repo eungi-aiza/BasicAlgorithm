a6ea43077eadb65a245bb5e2179e26556566958b
eungi-aiza <kuman5262a@gmail.com>
2023-04-13 14:35:32 +0900

    230413

---
+++ eunji/0x02/BOJ_1000.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int A; int B; int C;
+    cin >> A >> B;
+    C = A + B;
+    cout << C;
+}
