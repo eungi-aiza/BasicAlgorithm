765e590ae583af8bbd2c9ba279918b21f1088bd8
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-04-13 20:01:23 +0900

    Update and rename test.py to 10869.cpp

---
+++ chaeyun/10869.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cin>>i>>j;
+    cout << i+j << endl;
+    cout << i-j << endl;
+    cout << i*j << endl;
+    cout << i/j << endl;
+    cout << i%j << endl;
+}
