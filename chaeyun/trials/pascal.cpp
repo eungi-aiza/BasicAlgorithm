0aad103dc21610e35186c184850966ec3b6de4df
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-04-21 13:05:21 +0900

    Create pascal.cpp

---
+++ chaeyun/trials/pascal.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int combination(int n, int r){
+    if(n == r || r == 0) 
+        return 1; 
+    else 
+        return combination(n - 1, r - 1) + combination(n - 1, r);
+}
+
+vector<vector <int>> getPascal(int &n)
+{    
+    vector<vector<int>> v(n);  
+    int cb;
+    for (int i=0; i < n; i++){
+        // Initialize the vector at index i with size i+1
+        v[i].resize(i+1);
+        for(int j=0; j < i+1; j++){
+            cb = combination(i, j);
+            v[i][j] = cb;
+        }
+    }
+    return v;
+}
+
+void printPascal(vector<vector <int>> &p){
+    for (int k = 0; k < p.size(); k++) {
+        for (int l = 0; l < k+1; l++) {
+            cout << p[k][l] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int numRow;
+    cin >> numRow;
+    vector<vector <int>> pascal;
+    
+    pascal = getPascal(numRow);
+    printPascal(pascal);
+
+    return 0;
+} 
