9cb3c9b8672151d23c0d9af07954b7d109552aa8
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-04-30 20:52:29 +0900

    trial기록+다른풀이 추가

--- chaeyun/0x04/1406.cpp
+++ chaeyun/0x04/1406.cpp
@@ -1,3 +1,13 @@
+/*
+1st trial : 컴파일에러 (변수선언하고 ; 안붙임, && 써야 하는데 and... 바보
+2nd trial : 커서 왼쪽 얘를 지워야 하는데 오른쪽 지우는 코드 짜버림
+3rd trial : cin.ignore() 붙이니 잘 돌아감
+
+추가 : 
+- list<char> L 안에 string 집어넣는 방식이 push_back 말고도 iterator로 돌리는 방법도 있고
+- auto cursor 외에도 iterator의 자료형 지정해주는 방식도 있음 (단 자료형이 명확할 경우만 가능)
+*/
+
 #include <iostream>
 #include <list>
 using namespace std;
@@ -5,15 +15,25 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     string init;
     cin >> init;
     cin >> n;
     cin.ignore();
-    list<char> L(init.begin(), init.end());
-    auto cursor = L.end();
-    
+    list<char> L;
+
+    // option 2 : list<char> L(init.begin(), init.end());
+    for (auto c : init) {
+        L.push_back(c);
+    }
+
+    // option 2 : auto cursor = L.end();
+    list<char>::iterator cursor = L.end();
+    int n;
+    cin >> n;
+    cin.ignore();
+
     for (int i = 0; i < n; i++) {
         string s;
         getline(cin, s);
