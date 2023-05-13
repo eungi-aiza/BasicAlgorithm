71c67f6310b9b323451476f5463578de581834f0
golddohyun <dianecy1121@naver.com>
2023-05-13 13:41:52 +0900

    add samples

--- chaeyun/0x04/sample.cpp
+++ chaeyun/0x04/sample.cpp
@@ -32,3 +32,54 @@ int main() {
 
     return 0;
 }
+
+
+/*
+struct Node {
+    struct NODE *prev, *next;
+    int data;
+};
+
+// pre, nxt에 포인터 대신 배열 상의 인덱스를 저장함
+// pre, nxt에 -1이 있으면 값이 앞이나 뒤에 원소가 존재하지 않음을 의미
+// unused - 새로운 원소가 들어갈 수 있는 인덱스, 원소가 추가될 때 마다 하나씩 증가함
+// 0 : 시작 원소로 고정 (값이 들어가지 않고 단지 시작점을 나타내기 위한 더미 노드)
+
+const int MX = 1000005;
+int dat[MX], pre[MX], nxt[MX];
+int unused = 1; 
+
+fill(pre, pre+MX, -1);
+fill(nxt, nxt+MX, -1);
+
+void traverse() {
+    int cur = nxt[0];
+    while(cur != -1) {
+        cout << dat[cur] << ' ';
+        cur = nxt[cur];
+    }
+    cout << endl;
+}
+
+// 새로운 원소를 생성 -> 새 원소의 pre값에 삽입할 위치의 주소 대입
+// -> 새 원소의 nxt 값에 삽입할 위치의 다음 (nxt) 값을 대입
+// -> 삽입할 위치의 nxt값과 그 다음 원소의 pre값을 새 원소로 변경
+// -> unused 1증가
+void insert(int addr, int num) {
+    dat[unused] = num;
+    pre[unused] = addr;
+    nxt[unused] = nxt[addr];
+    if (nxt[addr] != -1)  pre[nxt[addr]] = unused;
+    nxt[addr] = unused;
+    unused ++;
+}
+
+void erase(int addr) {
+    pr = pre[addr];
+    nt = nxt[addr];
+    if (nxt[addr] != -1) pre[nt] = pr;
+    nxt[pr] = nt;
+}
+
+
+*/
