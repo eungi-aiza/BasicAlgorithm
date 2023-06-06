887a7b189e39403f3553352e4be2b88fc832be67
SangsooIm <imsangsoo@snu.ac.kr>
2023-06-07 08:35:42 +0900

    230606

---
+++ sangsoo/0x0C Backtracking/15654.cpp
@@ -0,0 +1,12 @@
+// N과 M(5)
+// N과 M (1) 과 같은데(permutation)
+// 다만 숫자가 1,2,3,.. 이 아니고 주어지는 숫자.
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int arr[10]; int isused[10];
+void func(int k){
+    if(k==m){
+        for(int i =0; i < m; i++)
+    }
+}
