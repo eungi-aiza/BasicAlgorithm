7a0b9c01e3ed2f06ab0bd778a99f471c4c64b6da
SangsooIm <imsangsoo@snu.ac.kr>
2023-07-15 20:34:43 +0900

    230715

---
+++ sangsoo/0x15 Hash/7785.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int n;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        
+    }
+
+}
